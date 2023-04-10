df03ef98acfc2fdd8125bd2cbc2ca9118c7b42a8
McKenzievdH <mckenziv@andrew.cmu.edu>
2023-04-10 13:29:39 -0400

    strm_join benches

--- fancy-garbling/benches/circuits.rs
+++ fancy-garbling/benches/circuits.rs
@@ -129,19 +129,78 @@ fn bench_eval_c17(c: &mut Criterion) {
     });
 }
 
-fn bench_garble_k8(c: &mut Criterion) {
-    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k8_v1_n3.txt");
-    c.bench_function("garble::k8", move |bench| {
+fn bench_garble_k6(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k6_v1_n1.txt");
+    c.bench_function("garble::strm_join_k6", move |bench| {
         bench.iter(|| garble::<WireMod2, _>(&circ));
     });
 }
-
+fn bench_eval_k6(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k6_v1_n1.txt");
+    let (en, gc) = garble::<WireMod2, _>(&circ).unwrap();
+    let gb = en.encode_garbler_inputs(&vec![0u16; 87]);
+    let ev = en.encode_evaluator_inputs(&vec![]);
+    c.bench_function("eval::strm_join_k6", move |bench| {
+        bench.iter(|| gc.eval(&circ, &gb, &ev));
+    });
+}
+fn bench_garble_k7(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k7_v1_n2.txt");
+    c.bench_function("garble::strm_join_k7", move |bench| {
+        bench.iter(|| garble::<WireMod2, _>(&circ));
+    });
+}
+fn bench_eval_k7(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k7_v1_n2.txt");
+    let (en, gc) = garble::<WireMod2, _>(&circ).unwrap();
+    let gb = en.encode_garbler_inputs(&vec![0u16; 87]);
+    let ev = en.encode_evaluator_inputs(&vec![]);
+    c.bench_function("eval::strm_join_k7", move |bench| {
+        bench.iter(|| gc.eval(&circ, &gb, &ev));
+    });
+}
+fn bench_garble_k8(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k8_v1_n3.txt");
+    c.bench_function("garble::strm_join_k8", move |bench| {
+        bench.iter(|| garble::<WireMod2, _>(&circ));
+    });
+}
 fn bench_eval_k8(c: &mut Criterion) {
     let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k8_v1_n3.txt");
     let (en, gc) = garble::<WireMod2, _>(&circ).unwrap();
     let gb = en.encode_garbler_inputs(&vec![0u16; 87]);
     let ev = en.encode_evaluator_inputs(&vec![]);
-    c.bench_function("eval::k8", move |bench| {
+    c.bench_function("eval::strm_join_k8", move |bench| {
+        bench.iter(|| gc.eval(&circ, &gb, &ev));
+    });
+}
+fn bench_garble_k9(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k9_v1_n6.txt");
+    c.bench_function("garble::strm_join_k9", move |bench| {
+        bench.iter(|| garble::<WireMod2, _>(&circ));
+    });
+}
+fn bench_eval_k9(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k9_v1_n6.txt");
+    let (en, gc) = garble::<WireMod2, _>(&circ).unwrap();
+    let gb = en.encode_garbler_inputs(&vec![0u16; 87]);
+    let ev = en.encode_evaluator_inputs(&vec![]);
+    c.bench_function("eval::strm_join_k9", move |bench| {
+        bench.iter(|| gc.eval(&circ, &gb, &ev));
+    });
+}
+fn bench_garble_k10(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k10_v1_n11.txt");
+    c.bench_function("garble::strm_join_k10", move |bench| {
+        bench.iter(|| garble::<WireMod2, _>(&circ));
+    });
+}
+fn bench_eval_k10(c: &mut Criterion) {
+    let circ = circuit("../../TFHE_repo/Garbled/circuits/strm_join/k10_v1_n11.txt");
+    let (en, gc) = garble::<WireMod2, _>(&circ).unwrap();
+    let gb = en.encode_garbler_inputs(&vec![0u16; 87]);
+    let ev = en.encode_evaluator_inputs(&vec![]);
+    c.bench_function("eval::strm_join_k10", move |bench| {
         bench.iter(|| gc.eval(&circ, &gb, &ev));
     });
 }
@@ -149,7 +208,8 @@ fn bench_eval_k8(c: &mut Criterion) {
 criterion_group! {
     name = parsing;
     config = Criterion::default().warm_up_time(Duration::from_millis(100));
-    targets = bench_garble_c17, bench_eval_c17, bench_garble_k8, bench_eval_k8
+    targets = bench_garble_k6, bench_eval_k6, bench_garble_k7, bench_eval_k7, bench_garble_k8, bench_eval_k8,
+    bench_garble_k9, bench_eval_k9, bench_garble_k10, bench_eval_k10
 }
 
 criterion_main!(parsing);
